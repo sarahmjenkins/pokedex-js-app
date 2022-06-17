1b5ad3015f923c87eb6c074ee7d164fd5d0ca214
sarahmjenkins <107324271+sarahmjenkins@users.noreply.github.com>
2022-06-17 16:28:56 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 3 + 4;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
