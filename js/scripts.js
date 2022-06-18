9fad8b3e0c0755d45412bea70579e41c84f1919e
sarahmjenkins <107324271+sarahmjenkins@users.noreply.github.com>
2022-06-18 15:46:45 -0600

    Added pokemonList array to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello World');
-
-let favoriteFood = 'pasta';
-document.write(favoriteFood);
+//Creates the list of Pokemon that will be available in the Pokedex app.
+let pokemonList = [
+  {name: 'Snorlax', height: 2.1, type: 'normal'},
+  {name: 'Frillish', height: 1.2, type: ['water', 'ghost']},
+  {name: 'Cubchoo', height: 0.5, type: 'ice'},
+  {name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal']},
+  {name: 'Meganium', height: 1.8, type: 'grass'}
+];
