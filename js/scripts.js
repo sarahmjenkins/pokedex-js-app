c40c4abad6e0bcee7b6d194c116c98e6288e3967
sarahmjenkins <107324271+sarahmjenkins@users.noreply.github.com>
2022-06-26 16:16:39 -0600

    Updated how pokemon are displayed
    -Replaced document.write with DOM manipulation
    -Updated search/filter function
    -Added event handler for when pokemon are clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Creates the list of Pokemon that will be available in the Pokedex app. Includes IIFE from Task 1.5
+// Creates the list of Pokemon that will be available in the Pokedex app. Includes IIFE from Task 1.5
 let pokemonRepository = (function () {
   let pokemonList = [
     {name: 'Snorlax', height: 2.1, type: ['normal']},
@@ -7,53 +7,64 @@ let pokemonRepository = (function () {
     {name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal']},
     {name: 'Meganium', height: 1.8, type: ['grass']}
   ];
-//function to return pokemonList (Task 1.5)
+
+  // function to return pokemonList (Task 1.5)
   function getAll() {
     return pokemonList;
   }
-//function to add a pokemon to pokemonList if it is an object with the right keys (Task 1.5 bonus)
+
+  // function to add a pokemon to pokemonList if it is an object with the right keys (Task 1.5 bonus)
   function add(pokemon) {
     if (typeof pokemon === 'object' && Object.keys(pokemon) === ['name', 'height', 'type']) { 
       pokemonList.push(pokemon);
     }
   }
-//How to access pokemonRepository
+
+  // Task 1.6: create buttons with names of pokemon in pokemonList
+  function addListItem(pokemon) {
+    let unorderedList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    unorderedList.appendChild(listItem);
+    //function that logs pokemon details in console when clicked on
+    clickPokemon(button, pokemon);
+  }
+  
+  // Functions to create event handler that will log details of clicked on pokemon in the console
+  function clickPokemon(button, pokemon) {
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    });
+  }
+  
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  // Adding filter/search functionality
+  function search(query) {
+    return pokemonList.filter(pokemon => pokemon.name.toLowerCase() === query.toLowerCase());
+  }
+
+  // How to access pokemonRepository
   return {
     getAll: getAll,
-    add: add
-  }
+    add: add,
+    addListItem: addListItem,
+    search: search
+  };
 })();
 
-//From Task 1.5, Part 2: updated forEach() because of IIFE
-pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(`<p>${pokemon.name}, height: ${pokemon.height} meters`);
-  if (pokemon.height >= 2) {
-    document.write('<br>(Wow, that\'s big!)');
-  }
-  document.write('</p>');
+// Task 1.6: access pokemonRepository and create a button for each pokemon in the repository using their name
+pokemonRepository.getAll().forEach(function(pokemon) {
+ pokemonRepository.addListItem(pokemon);
 });
 
-//Task 1.5 bonus: filter by name. I got this far then I got stuck! 
-let result = pokemonRepository.getAll().filter(obj => obj.name === 'Snorlax');
-document.write(`<p class="filter">Here is the result of your search:<br> ${result}</p>`);
 
+// Accesses pokemonRepository to include details on searched for pokemon
+let result = pokemonRepository.search('Jigglypuff');
+  document.write(`<p class="filter">Here is the result of your search:<br> ${result[0].name}<br>${result[0].height} meters<br>Type: ${result[0].type}</p>`);
 
-
-//From Task 1.5, Part 1: forEach() function to take the place of for loop
-//pokemonList.forEach(function(pokemon) {
-//  document.write(`<p>${pokemon.name}, height: ${pokemon.height} meters`);
-//  if (pokemon.height >= 2) {
-//    document.write('<br>(Wow, that\'s big!)');
-//  }
-//  document.write('</p>');
-//});
-
-
-//From Task 1.3: loop lists names and heights of pokemon and adds note "Wow, that's big!" if the pokemon's height is greater than or equal to 2 meters.
-//for (let i=0; i<pokemonList.length; i++) {
-//  if (pokemonList[i].height >= 2) {
-//    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters<br>(Wow, that's big!)</p>`)
-//  } else {
-//    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters</p>`);
-//  }
-//};
