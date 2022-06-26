cb2ef04f6a037dae41df2c19262a67342a19b0ba
sarahmjenkins <107324271+sarahmjenkins@users.noreply.github.com>
2022-06-25 18:18:08 -0600

    Replaced for loop with forEach() function

--- js/scripts.js
+++ js/scripts.js
@@ -7,40 +7,24 @@ let pokemonList = [
   {name: 'Meganium', height: 1.8, type: ['grass']}
 ];
 
-//Loop lists names and heights of pokemon and adds note "Wow, that's big!" if the pokemon's height is greater than or equal to 2 meters.
 
-//This was the original way I wrote it, and it worked, but I wanted to try out addigning a variable like the tips at the end of the exercise talked about. (see below)
-
-for (let i=0; i<5; i++) {
-  if (pokemonList[i].height >= 2) {
-    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters<br>(Wow, that's big!)</p>`)
-  } else {
-    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters</p>`);
+//From Task 1.5: forEach() function to take the place of for loop
+pokemonList.forEach(function(pokemon) {
+  document.write(`<p>${pokemon.name}, height: ${pokemon.height} meters`);
+  if (pokemon.height >= 2) {
+    document.write('<br>(Wow, that\'s big!)');
   }
-}
-
-/* 
-Trying out creating a variable based on tip at the bottom said that "assigning variables can help with readability and avoid lines becoming too long"
-
-First attempt--problem is that bigPokemon becomes a true/false, so bigPokemon.name and bigPokemon.height is undefined bc value of bigPokemon is true or false
-
-for (let i=0; i<5; i++) {
-  let bigPokemon = pokemonList[i].height >= 2 
-  if (bigPokemon) {
-    document.write(`<p>${bigPokemon.name}, height: ${bigPokemon.height} meters<br>(Wow, that's big!)</p>`)
-  } else {
-    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters</p>`);
-  }
+  document.write('</p>');
+});
 
 
-Second attempt--This one works, but it seems like more work than the version above, so I'm going with the one that doesn't create the variable
 
-for (let i=0; i<5; i++) {
-  if (pokemonList[i].height >= 2) {
-    let bigPokemon = pokemonList[i];
-    document.write(`<p>${bigPokemon.name}, height: ${bigPokemon.height} meters<br>(Wow, that's big!)</p>`)
-  } else {
-    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters</p>`);
-  }
-}
-*/
+
+//From Task 1.3: loop lists names and heights of pokemon and adds note "Wow, that's big!" if the pokemon's height is greater than or equal to 2 meters.
+//for (let i=0; i<pokemonList.length; i++) {
+//  if (pokemonList[i].height >= 2) {
+//    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters<br>(Wow, that's big!)</p>`)
+//  } else {
+//    document.write(`<p>${pokemonList[i].name}, height: ${pokemonList[i].height} meters</p>`);
+//  }
+//};
